5899da2ca5101bac948e0f896f776903434c9c08
jkey-67 <jkeymer@gmx.net>
2021-03-18 21:37:20 +0100

    Installer for windows is working

---
+++ src/tools/install.py
@@ -0,0 +1,34 @@
+import PyInstaller
+import PyInstaller.__main__
+import os
+
+def instFiles():
+    
+    workdir = os.path.abspath(os.path.join(os.getcwd(),"..",".."))
+    #specfile = os.path.join(workdir, 'spyglass.spec')
+    devdir = workdir
+    distdir = os.path.join(devdir, 'dist')
+    builddir = os.path.join(devdir, 'build')
+    pathex = os.path.join(workdir, 'src' )
+    PyInstaller.__main__.run([
+        '--clean',
+        '-p', pathex,
+        '--distpath', distdir,
+        '--workpath', builddir,
+        '--onefile',
+        '--windowed',
+        '..\spyglass.py',
+        '--icon=..\icon.ico',
+        "--add-data", "../vi/ui/*.ui;./vi/ui",
+        "--add-data", "../vi/ui/res/*;./vi/ui/res",
+        "--add-data", "../vi/ui/res/styles/*;./vi/ui/res/styles",
+        "--add-binary", "../lib/*.dll;./lib"
+        '--hidden-import=pyttsx3.drivers',
+        '--hidden-import=pyttsx3.drivers.dummy',
+        '--hidden-import=import=pyttsx3.drivers.espeak',
+        '--hidden-import=pyttsx3.drivers.nsss',
+        '--hidden-import=pyttsx3.drivers.sapi5'
+    ])
+
+if __name__ == "__main__":
+    instFiles()
