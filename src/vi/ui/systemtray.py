61432e668cc134e2611f9061d42c131d21c8127b
jkey-67 <jkeymer@gmx.net>
2021-03-20 00:22:06 +0100

    Sound is now played with  QSoundEffect

--- src/vi/ui/systemtray.py
+++ src/vi/ui/systemtray.py
@@ -83,7 +83,7 @@ class TrayIcon(QtWidgets.QSystemTrayIcon):
     # Min seconds between two notifications
     MIN_WAIT_NOTIFICATION = 15
 
-    alarm_distance = pyqtSignal(int)
+    alarm_distance = pyqtSignal()
     change_frameless = pyqtSignal()
     quit_signal = pyqtSignal()
 
