38ff41b5c2d6e5f75dbd23aabe5be53b070549e7
jkey-67 <jkeymer@gmx.net>
2022-08-26 21:47:35 +0200

    installer version is now 1.6.3

--- src/tools/install.py
+++ src/tools/install.py
@@ -16,7 +16,7 @@ def instFiles():
         '--workpath', builddir,
         '--onefile',
         '--windowed',
-        '--name', 'spyglass-1.6.2',
+        '--name', 'spyglass-1.6.3',
         '../spyglass.py',
         '--icon=../icon.ico',
         "--add-data=../vi/ui/*.ui;./vi/ui",
