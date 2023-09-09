ad615e3cc79182ab1ebfefcb577fc44934aaae33
mccool <mccool@sikijo.spdns.eu>
2023-09-09 06:48:42 +0200

    v1.6.7

--- src/tools/install_exe.py
+++ src/tools/install_exe.py
@@ -16,7 +16,7 @@ def instFiles():
         '--workpath', builddir,
         '--onefile',
         '--windowed',
-        '--name', 'spyglass-1.6.6',
+        '--name', 'spyglass-1.6.7',
         '../spyglass.py',
         '--icon=../icon.ico',
         '--add-data=../vi/universe/*.json;./vi/universe',
